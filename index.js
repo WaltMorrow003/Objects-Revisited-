e83f05b7bdc1ad33910556bdf530fb5254131889
WaltMorrow <wmorrow283@noscihigh.org>
2021-03-25 20:09:03 +0000

    Objects

--- index.js
+++ index.js
@@ -1,34 +1,36 @@
 //Create a variable called myObject and assign 
 //an object to it.
-
+var myObject = sword
 
 
 
 //Add two properties of firstName and lastName
 //using dot notation
-
-
+var firstName ='Walter'
+var lastName = 'Morrow'
+var fullName = firstName + lastName
+console.log(person1.fullName)
 
 
 //Add two properties of hobbies and age 
 //using bracket notation
 //hobbies must be an array of at least 4 strings
-
+var myHobbies = [videogame,14; drawing,13; anime,8; sleeping,14;]
 
 
 
 //Print the myObject variable
-
+console.log(myObject)
 
 
 
 //Use a for in loop to print all of the keys of the object
-
+function (print.To.Console [myHobbies])
 
 
 
 ///Use a for in loop to print all of the values of the object
-
+function (print.To.Console (myHobbies[3]))
 
 
 
@@ -39,3 +41,4 @@ create an object that is about a specific car
 Include: year, make, model, color and an array of features that you want your car to have
 
 */
+var car = [1997; Ford; traveler; black;]
